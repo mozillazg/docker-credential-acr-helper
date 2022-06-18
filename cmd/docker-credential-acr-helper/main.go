9b0a8a4466c19793d52d8f5c8a2ce1de8774438b
mozillazg <mozillazg101@gmail.com>
2022-06-18 14:10:01 +0800

    add -v to show version info

--- cmd/docker-credential-acr-helper/main.go
+++ cmd/docker-credential-acr-helper/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	"github.com/docker/docker-credential-helpers/credentials"
 	"github.com/mozillazg/docker-credential-acr-helper/pkg/credhelper"
+	"github.com/mozillazg/docker-credential-acr-helper/pkg/version"
 )
 
+const versionInfo = `docker-credential-acr-helper
+Version:    %s
+Git commit: %s
+`
+
 func main() {
+	var versionFlag bool
+	flag.BoolVar(&versionFlag, "v", false, "print version and exit")
+	flag.Parse()
+	if versionFlag {
+		fmt.Printf(versionInfo, version.Version, version.GitCommit)
+		os.Exit(0)
+	}
+
 	credentials.Serve(credhelper.NewACRHelper())
 }
